edb3eb81c1c7c95c04e8f99114ffa90388fdbc24
ART_AI <ART_AI@DESKTOP-TO8QNV5.localdomain>
2022-03-16 08:52:21 +0000

    fixed dataloader and created models

---
+++ genolearn/models/regression.py
@@ -0,0 +1,7 @@
+from sklearn.linear_model          import LinearRegression
+from sklearn.neural_network        import MLPRegressor
+from sklearn.neighbors             import KNeighborsRegressor
+from sklearn.svm                   import SVR
+from sklearn.gaussian_process      import GaussianProcessRegressor
+from sklearn.tree                  import DecisionTreeRegressor
+from sklearn.ensemble              import RandomForestRegressor, AdaBoostRegressor
