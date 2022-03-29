83b31d36d431594bcd2b0c2976d2907c81671edd
ART_AI <ART_AI@DESKTOP-TO8QNV5.localdomain>
2022-03-29 11:09:49 +0100

    modified demo-scripts

--- demo-A-lr.py
+++ demo-A-lr.py
@@ -3,24 +3,28 @@ from   genolearn.dataloader import DataLoader
 from   genolearn.models.classification import LogisticRegression
 from   genolearn.logger import msg
 
+from   time import time
+
+import warnings
 import numpy as np
 import os
 
+warnings.filterwarnings('ignore')
 os.environ['PYTHONWARNINGS'] = 'ignore'
 
 msg('executing demo-A-lr.py')
 
-dataloader = DataLoader('data-low-memory', 'raw-data/meta-data.csv', 'Accession', 'Region', 'Year')
+dataloader = DataLoader('data', 'raw-data/meta-data.csv', 'Accession', 'Region', 'Year')
 
-fisher = dataloader.load_feature_selection('fisher-score.npz')
-orders = fisher.rank()
+orders = dataloader.load_feature_selection('fisher-score.npz').rank()
 
-kwargs = dict(class_weight = 'balanced', n_jobs = -1)
+kwargs = dict(class_weight = 'balanced', solver = 'saga', n_jobs = -1)
 
-K           = [100, 1000, 10000, 100000, 1000000]
-C           = [1e-2, 1e-1, 1e0, 1e1, 1e2]
+K           = [100, 1000, 10000, 100000, 1000000][:-1] # k = 1,000,000 takes far too long
+C           = [1e-2, 1e0, 1e2]
 
 predictions = []
+times       = []
 
 for year in reversed(range(2014, 2019)):
 
@@ -30,28 +34,37 @@ for year in reversed(range(2014, 2019)):
     X_train, Y_train, X_test, Y_test = dataloader.load_train_test(range(year, 2019), [2019], features = features)
     
     predictions_k = []
+    times_k       = []
 
     for k in K:
 
         msg(f'{year} {k:7d}')
 
-        predictions_depth = []
+        predictions_c = []
+        times_c       = []
 
         for c in C:
             
             msg(f'{year} {k:7d} {c:.1e}')
             predictions_seed = []
+            times_seed       = []
 
             for seed in range(10):
+                train = time()
                 model = LogisticRegression(C = c, random_state = seed, **kwargs)
                 model.fit(X_train[:,:k], Y_train)
-                predictions_seed.append(dataloader.decode(model.predict(X_test[:,:k])))
+                train = time() - train
+                test  = time()
+                pred  = model.predict(X_test[:,:k])
+                test  = time() - test
+                predictions_seed.append(dataloader.decode(pred))
+                times_seed.append((train, test))
 
-            predictions_depth.append(predictions_seed)
+            predictions_c.append(predictions_seed)
 
         msg('', inline = True, delete = len(C))
 
-        predictions_k.append(predictions_depth)
+        predictions_k.append(predictions_c)
 
     msg('', inline = True, delete = len(K))
 
--- demo-A-rf.py
+++ demo-A-rf.py
@@ -3,12 +3,14 @@ from   genolearn.dataloader import DataLoader
 from   genolearn.models.classification import RandomForestClassifier
 from   genolearn.logger import msg
 
+from   time import time
+
 import numpy as np
 import os
 
 msg('executing demo-A-rf.py')
 
-dataloader = DataLoader('data-low-memory', 'raw-data/meta-data.csv', 'Accession', 'Region', 'Year')
+dataloader = DataLoader('data', 'raw-data/meta-data.csv', 'Accession', 'Region', 'Year')
 
 fisher = dataloader.load_feature_selection('fisher-score.npz')
 orders = fisher.rank()
@@ -19,6 +21,7 @@ K           = [100, 1000, 10000, 100000, 1000000]
 max_depths  = range(5, 101, 5)
 
 predictions = []
+times       = []
 
 for year in reversed(range(2014, 2019)):
 
@@ -28,37 +31,49 @@ for year in reversed(range(2014, 2019)):
     X_train, Y_train, X_test, Y_test = dataloader.load_train_test(range(year, 2019), [2019], features = features)
     
     predictions_k = []
+    times_k       = []
 
     for k in K:
 
         msg(f'{year} {k:7d}')
 
         predictions_depth = []
+        times_depth       = []
 
         for max_depth in max_depths:
             
             msg(f'{year} {k:7d} {max_depth:2d}')
             predictions_seed = []
+            times_seed       = []
 
             for seed in range(10):
+                train = time()
                 model = RandomForestClassifier(max_depth = max_depth, random_state = seed, **kwargs)
                 model.fit(X_train[:,:k], Y_train)
-                predictions_seed.append(dataloader.decode(model.predict(X_test[:,:k])))
+                train = time() - train
+                test  = time()
+                pred  = model.predict(X_test[:,:k])
+                test  = time() - test
+                predictions_seed.append(dataloader.decode(pred))
+                times_seed.append((train, test))
 
             predictions_depth.append(predictions_seed)
+            times_depth.append(times_seed)
 
         msg('', inline = True, delete = len(max_depths))
 
         predictions_k.append(predictions_depth)
+        times_k.append(times_depth)
 
     msg('', inline = True, delete = len(K))
 
     predictions.append(predictions_k)
-    
+    times.append(times_k)
+
 outdir = 'script-output'
 os.makedirs(outdir, exist_ok = True)
 
-np.savez_compressed(f'{outdir}/random-forest.npz', predictions = predictions, K = K, max_depths = max_depths)
+np.savez_compressed(f'{outdir}/random-forest.npz', predictions = predictions, times = times, K = K, max_depths = max_depths)
 
 create_log(outdir, 'demo-A-rf.txt')
 
